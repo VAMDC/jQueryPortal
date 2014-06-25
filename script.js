dfd6809dad98b6e494d5280ec1750d7e86a6a9e7
Thomas Marquart <thomas@marquart.se>
2014-06-25 19:12:31 +0200

    still getting the basics together

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
     $('#go').click(function(){
         var query = $('input[name=querystring]').val();
-        var url = 'http://vald.astro.uu.se/atoms-12.07/tap/sync?QUERY=select+all+where+AtomSymbol+%3D+%27U%27'
+        var url = 'http://vald.astro.uu.se/atoms-12.07/tap/sync?QUERY=select+all+where+AtomSymbol+%3D+%27U%27';
         var request = $.ajax({
-url: url,
-type: "GET",
-});
-request.done(function(data, textStatus, jqXHR ) {
-        var headers = jqXHR.getAllResponseHeaders()
-        var headers = jqXHR.getResponseHeader('VAMDC-COUNT-ATOMS')
-        $('ul#results').append('<li>'+ data + headers +'</li>')
-});
-request.fail(function( jqXHR, textStatus ) {
-alert( "Request failed: " + textStatus );
-});
+            url: url,
+            type: "HEAD",
+            beforeSend: function(xhr){xhr.setRequestHeader('VAMDC', 'vamdc');},
+            });
+        request.done(function(data, textStatus, jqXHR ){
+            var header = jqXHR.getResponseHeader('X-VAMDC-COUNT-ATOMS')
+            var header = jqXHR.getAllResponseHeaders()
+            $('ul#results').append('<li>'+ header +'</li>')
+        });
+        request.fail(function( jqXHR, textStatus ){
+            alert( "Request failed: " + textStatus );
         });
     });
+
+    $('#infopop').click(function(){
+        $('#infopop').popover('toggle')
+    });
+
+});
