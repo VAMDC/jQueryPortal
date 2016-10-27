0f64e6ea0864972bf2ecaf0133b480b4dbc9ceea
Thomas Marquart <thomas@marquart.se>
2016-10-27 13:35:30 +0200

    work on ionization slider

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 $(document).ready(function(){
-    var valdurl = 'http://vald.astro.uu.se/atoms-12.07/tap/sync';
-    var processurl = 'http://vamdc2.tmy.se/newproc/specsynth/service';
-    $('#selel').html('Be');
-    function hideall(){
-        $('#transitionsbox').hide();
-        $('#statesbox').hide();
-        $('#plotbox').hide();
-        $('#buttons2').hide();
-    }
-    hideall();
-    $('#statsbox').hide();
+    var url = 'http://vald.astro.uu.se/atoms-12.07/tap/sync';
+    $('#selel').html('Li');
 
     $('#go').click(function(){
-        hideall();
         var element = $('#selel').html();
         var lowave = $('#lower-input').val()
         var upwave = $('#upper-input').val()
-        var querystring = 'select all where AtomSymbol = \''+element+
-            '\' and RadTranswavelength < '+ upwave +
-            ' and RadTranswavelength > ' + lowave;
-        $('#querystring').text(querystring);
-
         var request = $.ajax({
-            url: valdurl,
-            data: {'QUERY':querystring},
+            url: url,
+            data: {'QUERY':'select all where AtomSymbol = \''+element+
+                '\' and RadTranswavelength < '+ upwave + 
+                ' and RadTranswavelength > ' + lowave},
             type: "HEAD",
             beforeSend: function(xhr){xhr.setRequestHeader('VAMDC', 'vamdc');},
             });
@@ -32,102 +19,24 @@ $(document).ready(function(){
             var states = jqXHR.getResponseHeader('VAMDC-COUNT-STATES')
             var transitions = jqXHR.getResponseHeader('VAMDC-COUNT-RADIATIVE')
             var size = jqXHR.getResponseHeader('VAMDC-APPROX-SIZE')
-            $('ul#results > li').each(function(){
-                this.style.color = "#999";
-            });
             $('ul#results').append('<li>'+ element + ' ('+lowave+'-'+upwave+'&Aring;): ' +
                 transitions + ' transitions between ' +
                 states +' states in '+ ions + ' ions/isotopes.'
                 +' ('+size+'MB)</li>')
-            $('#statsbox').show(200);
-            $('#buttons2').show();
-            $('html, body').animate({scrollTop: $("#go").offset().top}, 1000);
-
         });
         request.fail(function( jqXHR, textStatus ){
             alert( "Request failed: " + textStatus );
         });
     });
 
-    var options = {
-			lines: {
-				show: true,
-			},
-			points: {
-				show: true
-			},
-			xaxis: {
-				//tickDecimals: 0,
-				//tickSize: 1,
-                //autoscaleMargin: 0.002
-			},
-        selection: {
-				mode: "x"
-			}
-		};
-
-
-    $('#plotbtn').click(function(){
-        hideall();
-        var queryurl=valdurl + '?QUERY=' + escape($('#querystring').text());
-        //console.log(queryurl);
-        var request = $.ajax({
-            //url:'http://localhost:8001/specsynth/result/spec_21.json',
-            //url:'http://localhost:8001/specsynth/service',
-            url:processurl,
-            type:"POST",
-            data: {'url':queryurl},
-            dataType: "json",
-			success: PlotData,
-        });
-        $('#plot1').empty();
-        $('#plotbox').show(200);
-        $('html, body').animate({scrollTop: $("#go").offset().top}, 1000);
-    });
-
-
-    var placeholder = $("#plot1");
-    var data = [];
-    var plot = $.plot(placeholder, data, options);
-
-    function PlotData(rdata){
-        data = [];
-        for (var i = 0; i < rdata.length; i++) {
-            data.push([rdata[i][0],0.0])
-            data.push(rdata[i]);
-            data.push([rdata[i][0],0.0])
-        }
-        plot = $.plot(placeholder, [data], options);
-    }
-
-	placeholder.bind("plotselected", function (event, ranges) {
-
-		$("#selection").text(ranges.xaxis.from.toFixed(1) + " to " + ranges.xaxis.to.toFixed(1));
-
-		$.each(plot.getXAxes(), function(_, axis) {
-			var opts = axis.options;
-			opts.min = ranges.xaxis.from;
-			opts.max = ranges.xaxis.to;
-		});
-		plot.setupGrid();
-		plot.draw();
-		plot.clearSelection();
-    });
-
-	placeholder.dblclick(function () {
-		plot = $.plot(placeholder, [data], options);
-	});
-
-
     $('#fetch').click(function(){
-        hideall();
         var element = $('#selel').html();
         var lowave = $('#lower-input').val()
         var upwave = $('#upper-input').val()
         var request = $.ajax({
-            url: valdurl,
+            url: url,
             data: {'QUERY':'select all where AtomSymbol = \''+element+
-                '\' and RadTranswavelength < '+ upwave +
+                '\' and RadTranswavelength < '+ upwave + 
                 ' and RadTranswavelength > ' + lowave},
             type: "GET",
             beforeSend: function(xhr){xhr.setRequestHeader('VAMDC', 'vamdc');},
@@ -149,9 +58,6 @@ $(document).ready(function(){
                 $('table#transtab').append('<tr><td>'+$(this).find('Wavelength Value').first().text()+'</td><td>'+$(this).find('Log10WeightedOscillatorStrength Value').text()+'</td><td>'+$(this).find('LowerStateRef').text()+'</td><td>'+$(this).find('UpperStateRef').text()+'</td></tr>');
 
             });
-            $('#transitionsbox').show(200);
-            $('#statesbox').show();
-            $('html, body').animate({scrollTop: $("#transitionsbox").offset().top}, 1000);
         });
         request.fail(function( jqXHR, textStatus ){
             alert( "Request failed: " + textStatus );
@@ -167,7 +73,7 @@ $(document).ready(function(){
     $('#my_periodic').pte({ data : 'jquery.pte.lite.json', config : {size:34,spacing:2,x:60}, clickHandler : select_element });
 
 $("#waveslider").noUiSlider({
-    start: [500, 1650],
+    start: [400, 1000],
     step: 1,
     margin: 1,
     connect: true,
@@ -189,10 +95,43 @@ $("#waveslider").noUiSlider({
             })
         ],
         format: {
-            mark: ',',
             decimals: 0,
         }
     }
+});
+var customToolTip = $.Link({
+    target: '-tooltip-<div class="tooltip top" role="tooltip"></div>',
+    method: function ( value ) {
+        // The tooltip HTML is 'this', so additional
+        // markup can be inserted here.
+        $(this).html('<div class="tooltip-arrow"></div>'+
+            '<div class="tooltip-inner">'+value+'</div>'
+        );
+    }
+});
+
+$("#ionslider").noUiSlider({
+    start: [0, 999],
+    step: 1,
+    connect: true,
+    orientation: 'horizontal',
+    behaviour: 'tap-drag',
+    range: {
+        'min': 0,
+        '10%': 1,
+        '20%': 2,
+        '30%': 3,
+        '40%': 4,
+        '50%': 5,
+        '60%': 6,
+        '70%': 7,
+        '80%': 8,
+        'max': 999
+    },
+    serialization: {
+        lower: [ customToolTip ],
+        upper: [ customToolTip ]
+    },
 });
 
 });
